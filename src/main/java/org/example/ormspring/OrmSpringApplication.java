f5066a38780e10855a59f60c960672fc69eaab4d
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-11-22 21:19:30 -0300

    Update OrmSpringApplication.java

--- src/main/java/org/example/ormspring/OrmSpringApplication.java
+++ src/main/java/org/example/ormspring/OrmSpringApplication.java
@@ -1,5 +1,8 @@
 package org.example.ormspring;
 
+import org.example.ormspring.entidades.Funcionario;
+import org.example.ormspring.repositorios.FuncionarioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrmSpringApplication implements CommandLineRunner {
 
+    @Autowired
+    private FuncionarioRepo funcionarioRepo;
+
     public static void main(String[] args) {
         SpringApplication.run(OrmSpringApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Rolando Caio da Rocha");
+        funcionarioRepo.save(funcionario);
     }
 }
